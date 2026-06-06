400ce1537e4cf6353bc8b08c481cb7855d207f27
regissimao-unifor <regis.simao@unifor.br>
2026-06-06 18:05:47 -0300

    Update app.js

--- supabase_backend/app.js
+++ supabase_backend/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express'); 
 const supabaseClient = require('@supabase/supabase-js');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
